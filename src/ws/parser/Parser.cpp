1d6463c1b02aa35fca05a195b918332060952c0a
Hazurl <ricoujules69@gmail.com>
2018-08-26 13:25:15 +0200

    [Parser] Takes care about precedence

--- src/ws/parser/Parser.cpp
+++ src/ws/parser/Parser.cpp
@@ -9,8 +9,10 @@
 namespace ws { namespace parser {
 
 std::unique_ptr<AST> parse_number(std::size_t& index, std::vector<Token> const& tokens);
+std::unique_ptr<AST> parse_term(std::size_t& index, std::vector<Token> const& tokens);
 std::unique_ptr<AST> parse_expression(std::size_t& index, std::vector<Token> const& tokens);
-std::unique_ptr<AST> parse_binary_operation(std::size_t& index, std::vector<Token> const& tokens, std::unique_ptr<AST> lhs);
+std::unique_ptr<AST> parse_low_binary_operation(std::size_t& index, std::vector<Token> const& tokens);
+std::unique_ptr<AST> parse_high_binary_operation(std::size_t& index, std::vector<Token> const& tokens);
 
 std::optional<std::string> parse(std::vector<Token> const& tokens) {
     std::size_t i = 0;
@@ -32,7 +34,7 @@ std::unique_ptr<AST> parse_number(std::size_t& index, std::vector<Token> const& 
     auto& token = tokens.at(index++);
     std::cout << "parse_number # index: " << index << '\n';
 
-    if (token.type != TokenType::Litteral) 
+    if (token.type != TokenType::Litteral || token.subtype != TokenSubType::Float) 
         return std::cout << "parse_number = nullptr\n", nullptr;
 
     return std::cout << "parse_number = <number>\n", std::make_unique<Number>(
@@ -40,61 +42,111 @@ std::unique_ptr<AST> parse_number(std::size_t& index, std::vector<Token> const& 
     );
 }
 
-std::unique_ptr<AST> parse_expression(std::size_t& index, std::vector<Token> const& tokens) {
-    auto number = parse_number(index, tokens);
-    std::cout << "parse_expression # index: " << index << '\n';
+std::unique_ptr<AST> parse_term(std::size_t& index, std::vector<Token> const& tokens) {
+    auto& token = tokens.at(index);
+    std::cout << "parse_term # index: " << index << '\n';
 
-    if (!number)
-        return std::cout << "parse_expression = nullptr\n", nullptr;
-
-    try {
-        if (index >= tokens.size())
-            return std::cout << "parse_expression = <number>\n", std::move(number);
-
-        auto& token = tokens.at(index);
-        std::cout << "parse_expression ## index: " << index << '\n';
-
-        if (token.type != TokenType::Operator) 
-            return std::cout << "parse_expression = nullptr\n", nullptr;
-
-        return std::cout << "parse_expression = <parse_binary_operation...>\n", parse_binary_operation(index, tokens, std::move(number));
-    } catch(std::out_of_range const&) {
-        return std::cout << "parse_expression (/!\\ out_of_range) = nullptr\n", nullptr;
+    switch(token.type) {
+        case TokenType::Litteral: 
+            if (token.subtype == TokenSubType::Float)
+                return std::cout << "parse_term = <number>\n", parse_number(index, tokens);
+            else 
+                return std::cout << "parse_term = nullptr\n", nullptr;
+        default:
+            return std::cout << "parse_term = nullptr\n", nullptr;
     }
 }
 
-std::unique_ptr<AST> parse_binary_operation(std::size_t& index, std::vector<Token> const& tokens, std::unique_ptr<AST> lhs) {
-    auto& token = tokens.at(index++);
-    std::cout << "parse_binary_operation # index: " << index << '\n';
+std::unique_ptr<AST> parse_expression(std::size_t& index, std::vector<Token> const& tokens) {
+    return parse_low_binary_operation(index, tokens);
+}
 
-    if (token.type != TokenType::Operator) 
-        return std::cout << "parse_binary_operation = nullptr\n", nullptr;
+std::unique_ptr<AST> parse_low_binary_operation(std::size_t& index, std::vector<Token> const& tokens) {
+    std::unique_ptr<AST> lhs = parse_high_binary_operation(index, tokens);
 
-    std::string name;
-    switch(token.subtype) {
-        case TokenSubType::Plus:
-            name = "plus"; break;
-        case TokenSubType::Minus:
-            name = "minus"; break;
-        case TokenSubType::Multiplication:
-            name = "multiplication"; break;
-        case TokenSubType::Division:
-            name = "division"; break;
-        default:
-            return std::cout << "parse_binary_operation (/!\\ JSON corrupted) = nullptr\n", nullptr;
+    if (index >= tokens.size())
+        return std::move(lhs);
+
+    auto* token = &tokens.at(index);
+    std::cout << "parse_low_binary_operation # index: " << index << '\n';
+
+    while(token->type == TokenType::Operator && (token->subtype == TokenSubType::Plus || token->subtype == TokenSubType::Minus)) {
+        index++;
+        std::string name;
+        switch(token->subtype) {
+            case TokenSubType::Plus:
+                name = "plus"; break;
+            case TokenSubType::Minus:
+                name = "subtract"; break;
+            default:
+                return std::cout << "parse_low_binary_operation (/!\\ JSON corrupted) = nullptr\n", nullptr;
+        }
+
+        auto rhs = parse_high_binary_operation(index, tokens);
+        std::cout << "parse_low_binary_operation ## index: " << index << '\n';
+
+        if (!rhs || !lhs)
+            return std::cout << "parse_low_binary_operation = nullptr\n", nullptr;
+
+        std::cout << "parse_low_binary_operation = <" + name + ">\n";
+        lhs = std::make_unique<BinaryOperator>(
+            name,
+            std::move(lhs),
+            std::move(rhs)
+        );
+
+        if (index >= tokens.size())
+            return std::move(lhs);
+            
+        token = &tokens.at(index);
+        std::cout << "parse_low_binary_operation # index: " << index << '\n';
     }
 
-    auto rhs = parse_expression(index, tokens);
-    std::cout << "parse_binary_operation ## index: " << index << '\n';
+    return std::move(lhs);
+}
 
-    if (!rhs || !lhs)
-        return std::cout << "parse_binary_operation = nullptr\n", nullptr; 
+std::unique_ptr<AST> parse_high_binary_operation(std::size_t& index, std::vector<Token> const& tokens) {
+    std::unique_ptr<AST> lhs = parse_term(index, tokens);
 
-    return std::cout << "parse_binary_operation = <" + name + ">\n", std::make_unique<BinaryOperator>(
-        name,
-        std::move(lhs),
-        std::move(rhs)
-    );
+    if (index >= tokens.size())
+        return std::move(lhs);
+
+    auto* token = &tokens.at(index);
+    std::cout << "parse_low_binary_operation # index: " << index << '\n';
+
+    while(token->type == TokenType::Operator && (token->subtype == TokenSubType::Division || token->subtype == TokenSubType::Multiplication)) {
+        index++;
+        std::string name;
+        switch(token->subtype) {
+            case TokenSubType::Division:
+                name = "division"; break;
+            case TokenSubType::Multiplication:
+                name = "multiplication"; break;
+            default:
+                return std::cout << "parse_high_binary_operation (/!\\ JSON corrupted) = nullptr\n", nullptr;
+        }
+
+        auto rhs = parse_term(index, tokens);
+        std::cout << "parse_high_binary_operation ## index: " << index << '\n';
+
+        if (!rhs || !lhs)
+            return std::cout << "parse_high_binary_operation = nullptr\n", nullptr;
+
+        std::cout << "parse_high_binary_operation = <" + name + ">\n";
+        lhs = std::make_unique<BinaryOperator>(
+            name,
+            std::move(lhs),
+            std::move(rhs)
+        );
+
+        if (index >= tokens.size())
+            return std::move(lhs);
+            
+        token = &tokens.at(index);
+        std::cout << "parse_low_binary_operation # index: " << index << '\n';
+    }
+
+    return std::move(lhs);
 }
 
 }}
