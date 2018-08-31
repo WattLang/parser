230ec36e2051d4026d4c71437a26c0c6cfa7a936
Hazurl <ricoujules69@gmail.com>
2018-08-31 20:46:38 +0200

    [Token] Add Constructor

--- include/ws/parser/Token.hpp
+++ include/ws/parser/Token.hpp
@@ -9,6 +9,8 @@ namespace ws { namespace parser {
 class Token {
 public:
 
+    Token(std::string const& content, TokenType type, TokenSubType subtype) : content(content), type(type), subtype(subtype) {}
+
     std::string content;
     TokenType type;
     TokenSubType subtype;
