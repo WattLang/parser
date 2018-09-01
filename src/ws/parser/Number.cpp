eeeb06f187defa9fb09f8b93a2473ecff1e55ae7
Hazurl <ricoujules69@gmail.com>
2018-09-01 18:43:05 +0200

    [AST-Number] Fix "type" to "literal.float"

--- src/ws/parser/Number.cpp
+++ src/ws/parser/Number.cpp
@@ -7,7 +7,7 @@ Number::Number(std::string const& value) : value(value) {}
 std::string Number::compile(unsigned indentation) const {
     return 
                                         "{\n" +
-        std::string(indentation, ' ') + "  type:\"litteral\",\n" +
+        std::string(indentation, ' ') + "  type:\"literal.float\",\n" +
         std::string(indentation, ' ') + "  value:\"" + value + "\"\n" +
         std::string(indentation, ' ') + "}";
 }
