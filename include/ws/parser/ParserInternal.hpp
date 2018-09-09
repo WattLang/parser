4fbdc235bda8b1c351d8a995702267e63ed05ccf
Hazurl <ricoujules69@gmail.com>
2018-09-09 21:23:22 +0200

    [Parser] Let the code breathe

--- include/ws/parser/ParserInternal.hpp
+++ include/ws/parser/ParserInternal.hpp
@@ -66,7 +66,7 @@ namespace ws::parser {
  * map (B(A), Parser<A>) -> Parser<B>
  *    Run the callback on the result of the parser if it has succeeded
 
- * mapI (X(A...), Parser<std::tuple<A...>>) -> Parser<B>
+ * mapI (B(A...), Parser<std::tuple<A...>>) -> Parser<B>
  *    Equivalent of 'map' but unpack each element of a tuple
 
  * log (std::string, Parser<A>) -> Parser<A>
@@ -77,32 +77,19 @@ namespace ws::parser {
 
  */
 
-template<typename T>
-using Result = std::variant<ParserError, T>;
 
-template<typename T>
-using Parser = std::function<Result<T>(TokenStream&)>; 
 
-template<typename T>
-bool has_failed(Result<T> const& r) {
-    return std::holds_alternative<ParserError>(r);
-}
 
-template<typename T>
-Parser<T> try_(Parser<T> const& p) {
-    return [=] (TokenStream& it) -> Result<T> {
-        try {
-            auto backup = it;
-            auto res = p(backup);
-            if (!has_failed(res)) {
-                it = backup;
-            }
-            return std::move(res);
-        } catch (std::out_of_range&) {
-            return ParserError::error();
-        }
-    };
-}
+
+/*
+ * Internal Helpers
+ */
+
+namespace internal {
+
+
+
+
 
 template<typename T>
 struct is_tuple : std::bool_constant<false> {};
@@ -112,6 +99,12 @@ struct is_tuple<std::tuple<Ts...>> : std::bool_constant<true> {};
 
 template<typename T>
 constexpr auto is_tuple_v = is_tuple<T>::value;
+
+
+
+
+
+// Combine::combine will put both type in a tuple, nested tuples will be flatten
 
 template<typename A, typename B, bool = is_tuple_v<A>, bool = is_tuple_v<B>>
 struct Combine;
@@ -151,6 +144,10 @@ struct Combine<B, std::tuple<Args...>, false, true> {
 template<typename A, typename B>
 using Combine_t = typename Combine<A, B>::type;
 
+
+
+
+
 template<typename T>
 struct is_variant : std::bool_constant<false> {};
 
@@ -160,6 +157,16 @@ struct is_variant<std::variant<Ts...>> : std::bool_constant<true> {};
 template<typename T>
 constexpr auto is_variant_v = is_variant<T>::value;
 
+
+
+
+
+// Create a bigger variant from another
+// 'V' is the target variant
+// 'I' is the starting index, should be 0 at the start
+// 'Offset' between the target variant and the current variant, use in 'std::in_place_index<I + Offset>'
+// 'Ts...' are the types in the source variant
+
 template<typename V, std::size_t I, size_t Offset, typename...Ts>
 V create_variant(std::variant<Ts...>&& v) {
     if constexpr (I + 1 < sizeof...(Ts))
@@ -167,6 +174,12 @@ V create_variant(std::variant<Ts...>&& v) {
             return create_variant<V, I + 1, Offset, Ts...>(std::move(v));
     return V { std::in_place_index<I + Offset>, std::move(std::get<I>(v)) };
 }
+
+
+
+
+
+// Either::left or Either::right will construct the variant<A, B>, any nested variants will be flatten
 
 template<typename A, typename B, bool = is_variant_v<A>, bool = is_variant_v<B>>
 struct Either;
@@ -221,9 +234,94 @@ struct Either<std::variant<Args...>, std::variant<Brgs...>, true, true> {
 template<typename A, typename B>
 using Either_t = typename Either<A, B>::type;
 
+
+
+
+
+// Pass all elements of the tuple to the function
+
+template<typename F, typename T, size_t...Is >
+decltype(auto) apply_tuple(F&& f, T t, std::index_sequence<Is...>) {
+  return std::forward<F>(f)(std::move(std::get<Is>(t))...);
+}
+
+
+
+
+
+template<typename...Ts, std::size_t...Is>
+std::ostream& print_tuple(std::ostream& os, std::tuple<Ts...> const& tuple, std::index_sequence<Is...>) {
+    (std::initializer_list<int>){((os << (Is == 0 ? "" : ", ") << std::get<Is>(tuple)), 0)...};
+    return os;
+}
+
+
+
+
+
+};
+
+
+
+
+
+/*
+ * General definitions
+ */
+
+template<typename T>
+using Result = std::variant<ParserError, T>;
+
+template<typename T>
+using Parser = std::function<Result<T>(TokenStream&)>; 
+
+
+
+
+
+template<typename T>
+bool has_failed(Result<T> const& r) {
+    return std::holds_alternative<ParserError>(r);
+}
+
+
+
+
+
+/*
+ * try_ (Parser<A>) -> Parser<A>
+ *    Try to run the parser, if it fails rollback the stream where it was, still returns what the parser returned
+ */
+template<typename T>
+Parser<T> try_(Parser<T> const& p) {
+    return [=] (TokenStream& it) -> Result<T> {
+        try {
+            auto backup = it;
+            auto res = p(backup);
+            if (!has_failed(res)) {
+                it = backup;
+            }
+            return std::move(res);
+        } catch (std::out_of_range&) {
+            return ParserError::error();
+        }
+    };
+}
+
+
+
+
+
+/*
+ * operator & (Parser<A>, Parser<B>) -> Parser<std::tuple<A, B>>
+ *    Run sequencially both parser, returns their result in a tuple, or an error
+ *    Note:
+ *        If A or B is a tuple, it will concatenate the tuple
+ *        So operator & (Parser<std::tuple<A, B>>, Parser<std::tuple<C, A>>) -> Parser<std::tuple<A, B, C, A>>
+ */
 template<typename A, typename B>
-Parser<typename Combine<A, B>::type> operator & (Parser<A> const& pa, Parser<B> const& pb) {
-    return [=] (TokenStream& it) -> Result<typename Combine<A, B>::type> { 
+Parser<typename internal::Combine<A, B>::type> operator & (Parser<A> const& pa, Parser<B> const& pb) {
+    return [=] (TokenStream& it) -> Result<typename internal::Combine<A, B>::type> { 
         auto a = pa(it);
         if (has_failed(a))
             return ParserError::error();
@@ -232,27 +330,46 @@ Parser<typename Combine<A, B>::type> operator & (Parser<A> const& pa, Parser<B> 
         if (has_failed(b))
             return ParserError::error();
 
-        return Combine<A, B>::combine(std::move(std::get<A>(a)), std::move(std::get<B>(b)));
+        return internal::Combine<A, B>::combine(std::move(std::get<A>(a)), std::move(std::get<B>(b)));
     };
 }
 
+
+
+
+
+/*
+ * operator | (Parser<A>, Parser<B>) -> Parser<std::variant<A, B>>
+ *    Run the first parser, if it fails run the second parser, if it fails returns an error
+ *    Note:
+ *        If A or B is a variant, it will concatenate the variant
+ *        So operator | (Parser<std::variant<A, B>>, Parser<std::variant<C, A>>) -> Parser<std::variant<A, B, C, A>>
+ */
 template<typename A, typename B>
-Parser<typename Either<A, B>::type> operator | (Parser<A> const& pa, Parser<B> const& pb) {
-    return [=] (TokenStream& it) -> Result<typename Either<A, B>::type> { 
+Parser<typename internal::Either<A, B>::type> operator | (Parser<A> const& pa, Parser<B> const& pb) {
+    return [=] (TokenStream& it) -> Result<typename internal::Either<A, B>::type> { 
         auto a = try_(pa)(it);
 
         if (!has_failed(a))
-            return Either<A, B>::left(std::move(std::get<A>(a)));
+            return internal::Either<A, B>::left(std::move(std::get<A>(a)));
 
         auto b = try_(pb)(it);
 
         if (!has_failed(b))
-            return Either<A, B>::right(std::move(std::get<B>(b)));
+            return internal::Either<A, B>::right(std::move(std::get<B>(b)));
 
         return ParserError::error();
     };
 }
 
+
+
+
+
+/*
+ * map (B(A), Parser<A>) -> Parser<B>
+ *    Run the callback on the result of the parser if it has succeeded
+ */
 template<typename T, typename F>
 Parser<std::invoke_result_t<F, T>> map(F && f, Parser<T> const& p) {
     return [f, p] (TokenStream& it) -> Result<std::invoke_result_t<F, T>> {
@@ -263,21 +380,32 @@ Parser<std::invoke_result_t<F, T>> map(F && f, Parser<T> const& p) {
     };
 }
 
-template<typename F, typename T, size_t...Is >
-decltype(auto) apply_tuple(F&& f, T t, std::index_sequence<Is...>) {
-  return std::forward<F>(f)(std::move(std::get<Is>(t))...);
-}
 
+
+
+
+/*
+ * mapI (B(A...), Parser<std::tuple<A...>>) -> Parser<B>
+ *    Equivalent of 'map' but unpack each element of a tuple
+ */
 template<typename...Ts, typename F>
 Parser<std::invoke_result_t<F, Ts...>> mapI(F && f, Parser<std::tuple<Ts...>> const& p) {
     return [f, p] (TokenStream& it) -> Result<std::invoke_result_t<F, Ts...>> {
         auto r = p(it);
         if (has_failed(r)) 
             return std::get<ParserError>(r);
-        return apply_tuple(f, std::move(std::get<1>(r)), std::make_index_sequence<sizeof...(Ts)>());
+        return internal::apply_tuple(f, std::move(std::get<1>(r)), std::make_index_sequence<sizeof...(Ts)>());
     };
 }
 
+
+
+
+
+/*
+ * eat (TokenType, TokenSubType) -> Parser<Token>
+ *    Comsume the next token of the stream if the type/subtype match, or returns an error, the stream is always comsumed
+ */
 Parser<Token> eat(TokenType type, TokenSubType subtype) {
     return [=] (TokenStream& it) -> Result<Token> {
         auto t = *it;
@@ -289,6 +417,14 @@ Parser<Token> eat(TokenType type, TokenSubType subtype) {
     };
 }
 
+
+
+
+
+/*
+ * many (Parser<A>) -> Parser<std::vector<A>>
+ *    Accumulate the result of the parser until it fails, this parser never fails, since it can returns an empty vector
+ */
 template<typename T>
 Parser<std::vector<T>> many(Parser<T> const& p) {
     return [=] (TokenStream& it) {
@@ -302,59 +438,40 @@ Parser<std::vector<T>> many(Parser<T> const& p) {
     };
 }
 
+
+
+
+
+/*
+ * some (Parser<A>) -> Parser<std::tuple<A, std::vector<A>>>
+ *    Combination of itself and many, will parse A at least once, if the first iteration fails, an error is resturned
+ */
 template<typename T>
 Parser<std::tuple<T, std::vector<T>>> some(Parser<T> const& p) {
     return p & many(p);
 }
 
+
+
+
+
+/*
+ * join (Parser<std::variant<A...>>) -> Parser<B>
+ *    Run the parser and convert the result into B, All types in the variant need to be convertible to B
+ */
 template<typename T, typename...Ts>
 Parser<T> join (Parser<std::variant<T, Ts...>> const& parser) {
     return map([] (auto r) -> T { return std::visit([] (auto a) { return std::move(a); }, std::move(r)); }, parser);
 }
 
-template<typename...Ts, std::size_t...Is>
-std::ostream& print_tuple(std::ostream& os, std::tuple<Ts...> const& tuple, std::index_sequence<Is...>) {
-    (std::initializer_list<int>){((os << (Is == 0 ? "" : ", ") << std::get<Is>(tuple)), 0)...};
-    return os;
-}
 
-template<typename...Ts>
-std::ostream& operator<<(std::ostream& os, std::tuple<Ts...> const& tuple) {
-    os << "<";
-    return print_tuple(os, tuple, std::make_index_sequence<sizeof...(Ts)>()) << ">";
-}
 
-template<typename T>
-std::ostream& operator<<(std::ostream& os, std::vector<T> const& vector) {
-    bool first = true;
-    os << "[";
-    for(auto const& t : vector) {
-        if (!first)
-            os << ", ";
-        os << t;
-        first = false;
-    }
-    return os << "]";
-}
 
-template<typename T>
-std::ostream& operator<<(std::ostream& os, std::unique_ptr<T> const& ptr) {
-    return os << "*" << *ptr;
-}
 
-template<typename T>
-std::ostream& operator<<(std::ostream& os, std::optional<T> const& opt) {
-    if (opt)
-        return os << "$" << *opt;
-    return os << "$null";
-}
-
-template<typename...Ts>
-std::ostream& operator<<(std::ostream& os, std::variant<Ts...> const& variant) {
-    os << "|" << variant.index() << ": ";
-    return std::visit([&os] (auto const& v) -> std::ostream& { return os << v; }, variant) << "|";
-}
-
+/*
+ * log (std::string, Parser<A>) -> Parser<A>
+ *    Print a message when the parser is run, and when it succed/fail, returns the result of that parser, the type A need to implement operato<<(std::ostream&)
+ */
 template<typename T>
 Parser<T> log(std::string const& name, Parser<T> const& parser) {
     return [=] (TokenStream& it) {
@@ -371,6 +488,14 @@ Parser<T> log(std::string const& name, Parser<T> const& parser) {
     };
 }
 
+
+
+
+
+/*
+ * optional (Parser<A>) -> Parser<std::optional<A>>
+ *    Run the parser, if it fails return an empty optional, can't fail
+ */
 template<typename T>
 Parser<std::optional<T>> optional(Parser<T> const& p) {
     return [=] (TokenStream& it) -> Result<std::optional<T>> {
@@ -381,6 +506,17 @@ Parser<std::optional<T>> optional(Parser<T> const& p) {
     };
 }
 
+
+
+
+
+/*
+ * ref (Parser<A>) -> Parser<A>
+ *    By default parser are copied, but when you need recursion, you have to declare the parser first then referenced it with ref(parser)
+ *    Example:
+ *        Parser<Thing> parenthesis_expr;
+ *        parenthesis_expr = left_par & ref(parenthesis_expr) & right_par | expr;
+ */
 template<typename T>
 Parser<T> ref(Parser<T> const& p) {
     return [&p] (TokenStream& it) {
@@ -388,20 +524,118 @@ Parser<T> ref(Parser<T> const& p) {
     };
 }
 
+
+
+
+
+/*
+ * operator ~ (Parser<A>) -> Parser<A>
+ *    Equivalent of 'ref'
+ */
 template<typename T>
 Parser<T> operator~(Parser<T> const& p) {
     return ref(p);
 }
 
+
+
+
+
+/*
+ * operator > (Parser<A>, Parser<B>) -> Parser<B>
+ *    Equivalent of 'a & b' but discard the first result
+ */
 template<typename A, typename B>
 Parser<B> operator > (Parser<A> const& pa, Parser<B> const& pb) {
     return mapI([] (auto, auto res) { return res; }, pa & pb);
 }
 
 
+
+
+
+
+/*
+ * operator < (Parser<A>, Parser<B>) -> Parser<A>
+ *    Equivalent of 'a & b' but discard the second result
+ */
 template<typename A, typename B>
 Parser<A> operator < (Parser<A> const& pa, Parser<B> const& pb) {
     return mapI([] (auto res, auto) { return res; }, pa & pb);
 }
+
+
+
+
+
+/*
+ * operator<< for
+ *    std::tuple
+ *    std::optional
+ *    std::unique_ptr
+ *    std::variant
+ *    std::vector
+ */
+
+
+
+
+
+template<typename...Ts>
+std::ostream& operator<<(std::ostream& os, std::tuple<Ts...> const& tuple) {
+    os << "<";
+    return internal::print_tuple(os, tuple, std::make_index_sequence<sizeof...(Ts)>()) << ">";
+}
+
+
+
+
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, std::vector<T> const& vector) {
+    bool first = true;
+    os << "[";
+    for(auto const& t : vector) {
+        if (!first)
+            os << ", ";
+        os << t;
+        first = false;
+    }
+    return os << "]";
+}
+
+
+
+
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, std::unique_ptr<T> const& ptr) {
+    return os << "*" << *ptr;
+}
+
+
+
+
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, std::optional<T> const& opt) {
+    if (opt)
+        return os << "$" << *opt;
+    return os << "$null";
+}
+
+
+
+
+
+template<typename...Ts>
+std::ostream& operator<<(std::ostream& os, std::variant<Ts...> const& variant) {
+    os << "|" << variant.index() << ": ";
+    return std::visit([&os] (auto const& v) -> std::ostream& { return os << v; }, variant) << "|";
+}
+
+
+
+
 
 }
